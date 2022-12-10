bc38e55bbf3fa21b9a057e00abf3b575bcdfe71b
fedef14 <115482760+fedef14@users.noreply.github.com>
2022-12-10 22:47:12 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+  
+  int n = 0;
+  cin >> n;
+  
+  
+ float array[n];
+ 
+ for(int i=0;i<n;i++){
+ cin >> array[i];
+ }
+  
+  cout << massimo(array,n) << endl;
+  
   return 0;
 }
