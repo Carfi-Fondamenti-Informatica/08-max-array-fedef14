2d1a06236015d35d2c172ab019076c2872b52bb9
fedef14 <115482760+fedef14@users.noreply.github.com>
2022-12-10 22:42:30 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,15 @@
 
+#include "lib.h"
+float massimo(float array[],int n){
+float max = 0;
+  
+  for(int i=0;i<n;i++){
+  if(i == 0){
+  max = array[i];
+    }
+    if(array[i] > max){
+    max = array[i];
+    }
+  }
+  return max;
+}
